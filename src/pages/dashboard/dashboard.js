633708ffe414e846046c766d7ee2ad2ba248f3a1
Bozhidar Yordanov <bozhidar.h.yordanov@gmail.com>
2026-02-26 16:04:36 +0200

    Add dashboard page with layout and styling

---
+++ src/pages/dashboard/dashboard.js
@@ -0,0 +1,6 @@
+import './dashboard.css';
+import dashboardTemplate from './dashboard.html?raw';
+
+export const renderDashboardPage = () => dashboardTemplate;
+
+export const initDashboardPage = () => {};
